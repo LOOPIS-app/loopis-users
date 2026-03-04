028b85bff7284185c2a109ec67ddf64b53967369
HubertHilborn <hubert.hilborn@hotmail.com>
2026-03-04 13:25:10 +0100

    modified text

--- loopis-users.php
+++ loopis-users.php
@@ -6,8 +6,18 @@
 * Version: 0.0
 * Author: Hubert-Hilborn
 * Text Domain: loopis-users
-* License: <GNU:3 class="0"></GNU:3>
+* License: GPL-3.0-or-later
+* License URI: https://www.gnu.org/licenses/gpl-3.0.html
 */
+
+/*
+ * Copyright (C) 2026 LOOPIS association
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
 
 // Prevent direct access
 if (!defined('ABSPATH')) { 
